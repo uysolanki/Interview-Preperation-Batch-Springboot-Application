be1e6cbc83ef1ad37eb65e9930b1bfe8d3c082d3
Umar Solanki <uysolanki@gmail.com>
2024-09-25 20:08:49 +0530

    initial commit

---
+++ src/main/java/com/excelr/cms/InterviewPrepCmsApplication.java
@@ -0,0 +1,13 @@
+package com.excelr.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPrepCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPrepCmsApplication.class, args);
+	}
+
+}
