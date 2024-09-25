c33cf977b88da72b4fea26a6c6b98877ca6cdf1a
Umar Solanki <uysolanki@gmail.com>
2024-09-25 20:49:43 +0530

    made RESTful endpoints

--- src/main/java/com/excelr/cms/controller/CustomerController.java
+++ src/main/java/com/excelr/cms/controller/CustomerController.java
@@ -1,8 +1,59 @@
 package com.excelr.cms.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.excelr.cms.entity.Customer;
 
 @RestController
 public class CustomerController {
-
+	
+	@RequestMapping("/welcome")
+	public String welcome()
+	{
+		return "Welcome to Excler Edtech Pvt ltd";
+	}
+	
+	@RequestMapping("/getCustomer")  //RESTful endpoints , return a response JSON object
+	public Customer getCustomer()
+	{
+		Customer c1=new Customer();
+		c1.setCno(18);
+		c1.setUsername("Virat");
+		c1.setPassword("Virat123");
+		c1.setAge(35);
+		return c1;
+	}
+	
+	
+	@RequestMapping("/getListCustomers")  //RESTful endpoints , return a response JSON object
+	public ArrayList<Customer> getListCustomers()
+	{
+		Customer c1=new Customer();
+		c1.setCno(18);
+		c1.setUsername("Virat");
+		c1.setPassword("Virat123");
+		c1.setAge(35);
+		
+		Customer c2=new Customer();
+		c2.setCno(45);
+		c2.setUsername("Rohit");
+		c2.setPassword("Rohit123");
+		c2.setAge(37);
+		
+		
+		Customer c3=new Customer();
+		c3.setCno(1);
+		c3.setUsername("Rahul");
+		c3.setPassword("Rahul123");
+		c3.setAge(32);
+		
+		ArrayList<Customer> customers=new ArrayList();
+		customers.add(c1);
+		customers.add(c2);
+		customers.add(c3);
+		return customers;
+	}
 }
--- src/main/java/com/excelr/cms/repository/CustomerRepository.java
+++ src/main/java/com/excelr/cms/repository/CustomerRepository.java
@@ -1,8 +1,11 @@
 package com.excelr.cms.repository;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Repository
 public interface CustomerRepository {
 
+	
+	
 }
